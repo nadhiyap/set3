899d61a4473ccb099ac42de1b7ae6de195933598
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-03-03 15:44:38 +0530

    identi.py

---
+++ identi.py
@@ -0,0 +1,9 @@
+n=int(input("enter total"))
+b=[]
+for i in range(0,n):
+    a=input("enter number")
+    b.append(a)
+for i in range(0,len(b)):                                                                                                                                                                                       
+    if(i is b[i]):
+        print(i)
+        
