c357ab826dd9c28c5e8723df6ffcc13a97216e68
nadhiyap <36396218+nadhiyap@users.noreply.github.com>
2018-04-11 20:20:17 +0530

    arch.py

---
+++ arch.py
@@ -0,0 +1,10 @@
+n=int(input("enter a num1"))
+x=int(input("enter a num2"))
+l=[]
+for i in range(n):
+    a=int(input())
+    l.append(a)
+if l[0]+l[1]==x:
+    print("yes")
+else:
+    print("no")
